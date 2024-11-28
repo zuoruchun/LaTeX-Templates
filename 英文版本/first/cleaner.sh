9bc012415de8fa12d85b2e4c89140a1cfd236b73
zuoruchun <55564493+zuoruchun@users.noreply.github.com>
2024-11-28 09:09:11 +0800

    Delete 英文版本 directory

--- 英文版本/first/cleaner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Delete files with the specified extensions
-rm -f *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb *.synctex\(busy\)
-
-rm -f *.fls *.xdv *.fdb_latexmk
